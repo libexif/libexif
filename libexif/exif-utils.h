b5631f1c56c8b419c91081357acf41c3bc8a784f
Jörg Hoh <hohjg@users.sourceforge.net>
2004-04-01 22:24:22 +0200

    - use types of appropriate size if stdint.h is available

--- libexif/exif-utils.h
+++ libexif/exif-utils.h
@@ -27,6 +27,21 @@ extern "C" {
 
 #include <libexif/exif-byte-order.h>
 
+/* Works correct only on machines with a stdint.h, otherwise it assumes
+ * sizeof(long) == 4                                                    */
+  
+#ifdef HAVE_INTTYPES_H
+#include <stdint.h>
+typedef char		ExifByte;          /* 1 byte  */
+typedef char *		ExifAscii;
+typedef uint16_t	ExifShort;         /* 2 bytes */
+typedef uint32_t	ExifLong;          /* 4 bytes */
+typedef struct {ExifLong numerator; ExifLong denominator;} ExifRational;
+typedef char		ExifUndefined;     /* 1 byte  */
+typedef int32_t		ExifSLong;         /* 4 bytes */
+typedef struct {ExifSLong numerator; ExifSLong denominator;} ExifSRational;
+
+#else
 typedef char		ExifByte;          /* 1 byte  */
 typedef char *		ExifAscii;
 typedef unsigned short	ExifShort;         /* 2 bytes */
@@ -35,7 +50,7 @@ typedef struct {ExifLong numerator; ExifLong denominator;} ExifRational;
 typedef char		ExifUndefined;     /* 1 byte  */
 typedef signed long	ExifSLong;         /* 4 bytes */
 typedef struct {ExifSLong numerator; ExifSLong denominator;} ExifSRational;
-
+#endif
 
 ExifShort     exif_get_short     (const unsigned char *b, ExifByteOrder order);
 ExifLong      exif_get_long      (const unsigned char *b, ExifByteOrder order);
