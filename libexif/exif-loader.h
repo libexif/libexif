f5ac026a256e0c2e4f8f659dc3a740cb66982b3e
Hans Ulrich Niedermann <hun@n-dimensional.de>
2006-09-17 01:26:54 +0200

    nicer comments and parameter names

--- libexif/exif-loader.h
+++ libexif/exif-loader.h
@@ -39,43 +39,43 @@ typedef struct _ExifLoader ExifLoader;
  */
 ExifLoader *exif_loader_new     (void);
 /*! Allocate a new ExifLoader using an ExifMem
- *  \param[in] m the ExifMem
+ *  \param[in] mem the ExifMem
  *  \returns the allocated ExifLoader
  */
-ExifLoader *exif_loader_new_mem (ExifMem *);
+ExifLoader *exif_loader_new_mem (ExifMem *mem);
 /*! Increase the refcount of the ExifLoader
- *  \param[in] l the ExifLoader to increase the refcount of.
+ *  \param[in] loader the ExifLoader to increase the refcount of.
  */
-void        exif_loader_ref     (ExifLoader *);
+void        exif_loader_ref     (ExifLoader *loader);
 /*! Decrease the refcount of the ExifLoader
- *  \param[in] l the ExifLoader to decrease the refcount of.
+ *  \param[in] loader the ExifLoader to decrease the refcount of.
  *  If the refcount reaches 0, the ExifLoader is freeed.
  */
-void        exif_loader_unref   (ExifLoader *);
+void        exif_loader_unref   (ExifLoader *loader);
 
 /*! Write a file to the ExifLoader
- * \param[in] l the loader
+ * \param[in] loader the loader
  * \param[in] fname the path to the file to read
  */
-void        exif_loader_write_file (ExifLoader *, const char *fname);
+void        exif_loader_write_file (ExifLoader *loader, const char *fname);
 
 /*! Write a buffer to the ExifLoader
- * \param[in] l the loader to write too
- * \param[in] b the buffer to read from
- * \param[in] s the size of the buffer
+ * \param[in] loader the loader to write too
+ * \param[in] buf the buffer to read from
+ * \param[in] sz the size of the buffer
  * \returns 1 while EXIF data is read (or while there is still hope that there will be EXIF data later on), 0 otherwise.
  */
-unsigned char exif_loader_write (ExifLoader *l, unsigned char *b, unsigned int s);
+unsigned char exif_loader_write (ExifLoader *loader, unsigned char *buf, unsigned int sz);
 
 /*! Reset the ExifLoader
- * \param[in] l the loader
+ * \param[in] loader the loader
  */
-void          exif_loader_reset (ExifLoader *);
+void          exif_loader_reset (ExifLoader *loader);
 /*! Get an ExifData out of an ExifLoader
- * \param[in] l the loader
+ * \param[in] loader the loader
  * \returns and allocated ExifData
  */
-ExifData     *exif_loader_get_data (ExifLoader *);
+ExifData     *exif_loader_get_data (ExifLoader *loader);
 
 void exif_loader_log (ExifLoader *, ExifLog *);
 
