a37d5d3b613cfbcc9d7d28529f70e8b44041b3a1
Dan Fandrich <dan@coneharvesters.com>
2019-02-22 16:17:01 +0100

    Added test-extract.c and a new regression test extract-parse.sh.
    This extracts the EXIF tags from an image then compares the parsed value
    of the extracted tags with those of the original file. This ensures that
    the tags are written properly, without change in tag data. The MakerNote
    tag sometimes has a harmless, slight difference in size because of
    padding being removed.
    
    However, in developing this test, I found that the Olympus variant 4
    MakerNote has a huge size difference. This might be harmless (there
    might just be a lot of padding removed) but it's also possible that
    these MakerNotes aren't being properly parsed.  This discrepancy should
    be investigated.
    
    The exif_data_save_data() function is also returning some JPEG markers
    at the end of the buffer which I wasn't expecting.  This also should be
    investigated.
    
    The test is enabled anyway in the meantime to reduce the chance of
    regressions in the remaining tags.

---
+++ test/extract-parse.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Compares the parsed EXIF data extracted from test images with the parsed EXIF
+# data in the original images. This tests that the tag parsing and writing
+# round-trip produces an EXIF structure with the same meaning as the original.
+srcdir="${srcdir:-.}"
+TMPORIGINAL="$(mktemp)"
+TMPEXTRACTED="$(mktemp)"
+TMPDATA="$(mktemp)"
+trap 'rm -f "${TMPORIGINAL}" "${TMPEXTRACTED}" "${TMPDATA}"' 0
+
+# Remove the file name, which is a harmless difference between the two outputs.
+# Also delete the size of the MakerNote. Since the MakerNote is parsed
+# internally and rewritten, it can sometimes have slightly different padding
+# and therefore slightly different size, which is a semantically meaningless
+# difference.
+# FIXME: Not all MakerNote differences are harmless. For example,
+# olympus_makernote_variant_4.jpg has a huge size difference, probably because
+# of a parsing bug in libexif. This should be investigated. Ideally, this would
+# ignore small differences in size but trigger on larger differences.
+parse_canonicalize () {
+    sed \
+        -e '/^File /d' \
+        -e '/MakerNote (Undefined)$/{N;N;d}'
+}
+
+# Ensure that names are untranslated
+LANG=
+LANGUAGE=
+LC_ALL=C
+export LANG LANGUAGE LC_ALL
+for fn in "${srcdir}"/testdata/*.jpg ; do
+    ./test-parse "${fn}" | parse_canonicalize > "${TMPORIGINAL}"
+    ./test-extract -o "${TMPDATA}" "${fn}"
+    ./test-parse "${TMPDATA}" | parse_canonicalize > "${TMPEXTRACTED}"
+    if ! diff "${TMPORIGINAL}" "${TMPEXTRACTED}"; then
+        echo Error parsing "$fn"
+        exit 1
+    fi
+done
