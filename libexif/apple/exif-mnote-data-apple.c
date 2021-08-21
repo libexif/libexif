d61e9aaddf4f8af5aecea4935f8feb59f42141c1
Marcus Meissner <marcus@jet.franken.de>
2021-08-21 15:15:20 +0200

    added another integer overflow check (oss-fuzz)

--- libexif/apple/exif-mnote-data-apple.c
+++ libexif/apple/exif-mnote-data-apple.c
@@ -122,12 +122,17 @@ exif_mnote_data_apple_load(ExifMnoteData *md, const unsigned char *buf, unsigned
         d->entries[i].format = exif_get_short(buf + ofs + 2, d->order);
         d->entries[i].components = exif_get_long(buf + ofs + 4, d->order);
         d->entries[i].order = d->order;
+	if (buf_size / d->entries[i].components < exif_format_get_size(d->entries[i].format)) {
+		exif_log (md->log, EXIF_LOG_CODE_CORRUPT_DATA,
+                                  "ExifMnoteApplet", "Tag size overflow detected (components %lu vs size %u)", d->entries[i].components, buf_size);
+		break;
+	}
         dsize = exif_format_get_size(d->entries[i].format) * d->entries[i].components;
 	if ((dsize > 65536) || (dsize > buf_size)) {
 		/* Corrupt data: EXIF data size is limited to the
 		 * maximum size of a JPEG segment (64 kb).
 		 */
-		continue;
+		break;
 	}
         if (dsize > 4) {
             dofs = d->offset + exif_get_long(buf + ofs + 8, d->order);
