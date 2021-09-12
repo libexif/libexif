eb5dde463ec0efdabf0ecac42619b6235a4e8931
Marcus Meissner <marcus@jet.franken.de>
2021-09-12 15:15:25 +0200

    upgrade the limit a bit... eos m6 mark 2 has 156 mnote entries, add 100 as safety net

--- libexif/canon/exif-mnote-data-canon.c
+++ libexif/canon/exif-mnote-data-canon.c
@@ -225,8 +225,9 @@ exif_mnote_data_canon_load (ExifMnoteData *ne,
 	c = exif_get_short (buf + datao, n->order);
 	datao += 2;
 	/* Just use an arbitrary max tag limit here to avoid needing to much memory or time. There are 24 named tags currently.
+	 * current 2020 camera EOS M6 Mark 2 had 156 entries.
 	 * The format allows specifying the same range of memory as often as it can, so this multiplies quickly. */
-	if (c > 100) {
+	if (c > 250) {
 		exif_log (ne->log, EXIF_LOG_CODE_CORRUPT_DATA, "ExifMnoteCanon", "Too much tags (%d) in Canon MakerNote", c);
 		return;
 	}
