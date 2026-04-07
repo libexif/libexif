d317f291a51e42856ab5b131661122b6190e7824
Marcus Meissner <marcus@jet.franken.de>
2026-04-07 10:57:16 +0200

    added EXIF_TAG_IMAGE_DEPTH for https://github.com/libexif/libexif/issues/253

--- libexif/exif-tag.c
+++ libexif/exif-tag.c
@@ -435,6 +435,8 @@ static const struct TagEntry {
 	/* Not in EXIF 2.2 */
 	{EXIF_TAG_RELATED_IMAGE_FILE_FORMAT, "RelatedImageFileFormat",
 	 "RelatedImageFileFormat", "", ESL_UNKNOWN},
+	{EXIF_TAG_IMAGE_DEPTH, "ImageDepth",
+	 "ImageDepth", "", ESL_UNKNOWN},
 	/* Not in EXIF 2.2 */
 	{EXIF_TAG_RELATED_IMAGE_WIDTH, "RelatedImageWidth",
 	 "RelatedImageWidth", "", ESL_UNKNOWN},
