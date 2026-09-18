a7d8a3f41478dc910a023b11571e955fd5593cef
Marcus Meissner <meissner@suse.de>
2026-09-18 19:46:59 +0200

    fix divide by zero caused by last patch.

--- libexif/exif-data.c
+++ libexif/exif-data.c
@@ -183,7 +183,9 @@ exif_data_load_data_entry (ExifData *data, ExifEntry *entry,
 	/* {0,1,2,4,8} x { 0x00000000 .. 0xffffffff } 
 	 *   -> { 0x000000000 .. 0x7fffffff8 } */
 
-	if (entry->components >= (0xffffffff / exif_format_get_size(entry->format)))
+	if (	!exif_format_get_size(entry->format)	||
+		(entry->components >= (0xffffffff / exif_format_get_size(entry->format)))
+	)
 		return 0;
 
 	s = exif_format_get_size(entry->format) * entry->components;
